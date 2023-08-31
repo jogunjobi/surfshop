07aeb11ac3347792ac5cf6060af7648d7df9773d
Babajide Ogunjobi <jogunjobi@gmail.com>
2023-08-30 22:55:54 -0500

    added price to form and product list

--- products/forms.py
+++ products/forms.py
@@ -4,4 +4,4 @@ from .models import Product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['product_name', 'product_description', 'image']
+        fields = ['product_name', 'product_description', 'price', 'image']
